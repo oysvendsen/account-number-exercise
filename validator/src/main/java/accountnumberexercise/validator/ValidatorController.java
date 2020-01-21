d194d114b9b5eeb8f42edb29524f47bc1b1cf260
E216200 <oyvind.svendsen@evry.com>
2020-01-21 23:04:47 +0100

    Add RestController annotation and pingservice-mapping and validate-service-mapping to restcontroller. (I thought this was done).

--- validator/src/main/java/accountnumberexercise/validator/ValidatorController.java
+++ validator/src/main/java/accountnumberexercise/validator/ValidatorController.java
@@ -1,4 +1,22 @@
 package accountnumberexercise.validator;
 
+import com.evry.fs.enterprise.converter.accountnumber.CountryCode;
+import com.evry.fs.enterprise.core.validation.accountnumber.ValidateBban;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ValidatorController {
+
+    @GetMapping("/ping")
+    public void ping(){}
+
+    @GetMapping("/validate")
+    public boolean validate(String bban) {
+        if (ValidateBban.validatePattern(bban, CountryCode.NORWAY)) {
+            return ValidateBban.validateControlDigits(bban, CountryCode.NORWAY);
+        } else {
+            return false;
+        }
+    }
 }
