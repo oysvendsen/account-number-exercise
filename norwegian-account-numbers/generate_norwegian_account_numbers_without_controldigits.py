36d1ce296ce3c7bc1a18c06e4e4fbd13dae01de2
E216200 <oyvind.svendsen@evry.com>
2020-01-22 15:59:17 +0100

    Modify newAccNum-script to use '?' instead of '-' as single wildcard symbol.

--- norwegian-account-numbers/generate_norwegian_account_numbers_without_controldigits.py
+++ norwegian-account-numbers/generate_norwegian_account_numbers_without_controldigits.py
@@ -7,5 +7,5 @@ with open(filename, "w+") as outfile:
 		for series in bank_series_dict[bank]:
 			for i in range(1,n+1):
 				string_i = "{:04d}".format(i)
-				outfile.write(bank + series + string_i + "-\n")
+				outfile.write(bank + series + string_i + "?\n")
 				#print(bank + " " + series + " " + string_i)	
