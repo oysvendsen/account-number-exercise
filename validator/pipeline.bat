ef26d1220fb633f1958c30d23854fc26a55da974
E216200 <oyvind.svendsen@evry.com>
2020-01-28 17:50:08 +0100

    remove extra files

--- validator/pipeline.bat
+++
@@ -1,5 +0,0 @@
-set container-tag=account-number-exercise/validator
-
-copy ..\norwegian-account-numbers\*.txt src\main\resources\static\.
-mvn clean install
-heroku container:release web %container-tag%
